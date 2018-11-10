c870da62b6bda78bc2ff8a76b6ad504a82eedc93
mohit-patil <mohit_patil@outlook.in>
2018-11-10 23:24:32 +0530

    Added Splash Screen

--- app/src/main/java/com/example/mohit/moecho/SplashActivity.kt
+++ app/src/main/java/com/example/mohit/moecho/SplashActivity.kt
@@ -2,9 +2,13 @@ package com.example.mohit.moecho
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 
 class SplashActivity : AppCompatActivity() {
     var permissionsString = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -17,6 +21,39 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        if(!hasPermissions(this@SplashActivity,*permissionsString)){
+            //We have to Ask for Permissions
+            ActivityCompat.requestPermissions(this@SplashActivity,permissionsString,131)
+        }else{
+            Handlr(this@SplashActivity)
+
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            131 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Handlr(this@SplashActivity)
+                }else{
+                    Toast.makeText(this@SplashActivity,"Please Grant All the Permissions",Toast.LENGTH_SHORT).show()
+                    this.finish()
+                }
+                return
+            }
+            else ->{
+                Toast.makeText(this@SplashActivity,"Something went Wrong",Toast.LENGTH_SHORT).show()
+                this.finish()
+                return
+            }
+
+        }
     }
     fun hasPermissions(context: Context, vararg permissions: String): Boolean{
         var hasAllpermisiions = true
@@ -27,5 +64,12 @@ class SplashActivity : AppCompatActivity() {
             }
         }
         return hasAllpermisiions
+    }
+    fun Handlr(context: Context){
+        Handler().postDelayed({
+            val startAct = Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(startAct)
+            this.finish()
+        },1000)
     }
 }
