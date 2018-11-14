cb8bf644d9ac393b8a3f4edc62143ce1179b258d
mohit-patil <mohit_patil@outlook.in>
2018-11-14 23:52:51 +0530

    Main Screen Fragment(Incomplete)

--- app/src/main/java/com/example/mohit/moecho/fragments/MainScreenFragment.kt
+++ app/src/main/java/com/example/mohit/moecho/fragments/MainScreenFragment.kt
@@ -1,24 +1,85 @@
 package com.example.mohit.moecho.fragments
 
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.example.mohit.moecho.R
+import com.example.mohit.moecho.songs
+
 /**
  * A simple [Fragment] subclass.
  *
  */
 class MainScreenFragment : Fragment() {
 
+    var nowPlayingBottomBar: RelativeLayout? = null
+    var playPauseButton: ImageButton? = null
+    var songTitle: TextView? = null
+    var visibleLayout: RelativeLayout? = null
+    var noSongs: RelativeLayout? = null
+    var recyclerView: RecyclerView? = null
+    var myActivity: Activity? = null
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+        val view = inflater.inflate(R.layout.fragment_main_screen, container, false)
+        visibleLayout = view?.findViewById<RelativeLayout>(R.id.visibleLayout)
+        noSongs = view?.findViewById<RelativeLayout>(R.id.nosongs)
+        nowPlayingBottomBar = view?.findViewById<RelativeLayout>(R.id.hiddenbarmainscreen)
+        songTitle = view?.findViewById<TextView>(R.id.songTitleMainScreen)
+        playPauseButton = view?.findViewById<ImageButton>(R.id.playpausebutton)
+        recyclerView = view?.findViewById<RecyclerView>(R.id.contentMain)
+
+
+
+
+
+
+
+
+
+        return view
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        myActivity = context as Activity
+    }
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        myActivity = activity
+    }
+    fun getSongsFromPhone(): ArrayList<songs>{
+        var arrayList = ArrayList<songs>()
+        var contentResolver = myActivity?.contentResolver
+        var songuri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        var songCursor = contentResolver?.query(songuri,null,null,null,null)
+        if (songCursor != null && songCursor.moveToFirst()){
+            val songId = songCursor.getColumnIndex(MediaStore.Audio.Media._ID)
+            val songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE)
+            val songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            val songData = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA)
+            val dateIndex = songCursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED)
+            //while (songCursor.moveToNext())
+
+        }
+        TODO("return")
     }
 
 
